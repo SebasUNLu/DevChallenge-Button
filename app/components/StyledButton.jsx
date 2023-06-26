fa9801f5fa6af1a49be3f6ddf291907487724dfe
SebasUNLu <SebasUNLu@gmail.com>
2023-06-26 20:49:22 -0300

    US-01: disableShadow

--- app/components/StyledButton.jsx
+++ app/components/StyledButton.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
-const StyledButton = ({ text = 'Default' }) => {
+
+const StyledButton = ({ text = 'Default', disableShadow = false }) => {
+
+  const style = "text-[#3F3F3F] bg-[#E0E0E0] py-2 px-4 rounded-md hover:bg-[#AEAEAE] focus:bg-[#AEAEAE]"
+  const styleShadow = !disableShadow ? "drop-shadow-md" : ""
+
   return (
-    <button className='text-[#3F3F3F] bg-[#E0E0E0] py-2 px-4 rounded-md drop-shadow-md hover:bg-[#AEAEAE] focus:bg-[#AEAEAE]'>
+    <button className={`${style} ${styleShadow}`}>
       {text}
     </button>
   );
--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
-import Image from 'next/image'
-import StyledButton from './components/StyledButton'
+import Image from "next/image";
+import StyledButton from "./components/StyledButton";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <StyledButton />
+      <div className="w-9/12 flex flex-wrap gap-4">
+        <StyledButton />
+        <StyledButton disableShadow/>
+      </div>
     </main>
-  )
+  );
 }
