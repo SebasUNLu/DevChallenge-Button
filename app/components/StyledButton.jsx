2d2a53398541ea864b690ee9d56bdc8adbcdd3aa
SebasUNLu <SebasUNLu@gmail.com>
2023-06-26 22:51:02 -0300

    Ordeno un poco como se ven los botones

--- app/components/StyledButton.jsx
+++ app/components/StyledButton.jsx
@@ -5,22 +5,22 @@ import React from 'react';
 const theme = {
   colors: {
     default: {
-      solid: "text-[#3F3F3F] bg-[#E0E0E0] hover:bg-[#AEAEAE] focus:bg-[#AEAEAE]",
+      default: "text-[#3F3F3F] bg-[#E0E0E0] hover:bg-[#AEAEAE] focus:bg-[#AEAEAE]",
       outline: "bg-white hover:bg-[#2962FF1A] focus:bg-[#2962FF1A] text-[#3D5AFE] border-[#3D5AFE] border border-solid",
       text: "text-[#3D5AFE] bg-white hover:bg-[#2962FF1A] focus:bg-[#2962FF1A]"
     },
     primary: {
-      solid: "text-white bg-[#2962FF] hover:bg-[#0039CB] focus:bg-[#0039CB]",
+      default: "text-white bg-[#2962FF] hover:bg-[#0039CB] focus:bg-[#0039CB]",
       outline: "bg-white hover:bg-[#2962FF1A] focus:bg-[#2962FF1A] text-[#3D5AFE] border-[#3D5AFE] border border-solid",
       text: "text-[#3D5AFE] bg-white hover:bg-[#2962FF1A] focus:bg-[#2962FF1A]"
     },
     secondary: {
-      solid: "text-white bg-[#455A64] hover:bg-[#1C313A] focus:bg-[#1C313A]",
+      default: "text-white bg-[#455A64] hover:bg-[#1C313A] focus:bg-[#1C313A]",
       outline: "bg-white hover:bg-[#455A641A] focus:bg-[#455A641A] text-[#455A64] border border-solid border-[#455A64]",
       text: "bg-white hover:bg-[#455A641A] focus:bg-[#455A641A] text-[#455A64]"
     },
     danger: {
-      solid: "text-white bg-[#D32F2F] hover:bg-[#9A0007] focus:bg-[#9A0007]",
+      default: "text-white bg-[#D32F2F] hover:bg-[#9A0007] focus:bg-[#9A0007]",
       outline: "bg-white hover:bg-[#D32F2F1A] focus:bg-[#D32F2F1A] text-[#D32F2F] border border-solid border-[#D32F2F]",
       text: "bg-white hover:bg-[#D32F2F1A] focus:bg-[#D32F2F1A] text-[#D32F2F]"
     }
@@ -37,7 +37,7 @@ const theme = {
 }
 const variantTypes = ["solid", "outline", "text"]
 
-const StyledButton = ({ color = "default", variant = "solid", text = 'Default', disableShadow = false, disable = false }) => {
+const StyledButton = ({ color = "default", variant = "default", text = 'Default', disableShadow = false, disable = false }) => {
 
   const styleGeneral = "py-2 px-4 rounded-md font-sans font-medium text-sm";
 
@@ -48,15 +48,15 @@ const StyledButton = ({ color = "default", variant = "solid", text = 'Default', 
   else {
     // Reviso si los colores y variantes son correctos 
     // Colores --> default, primary, secondary, danger
-    // Variantes --> solid (default), outline, text
-    // Si los valores no son correctos, se les asigna los defaults (default y solid respectivamente)
+    // Variantes --> default, outline, text
+    // Si los valores no son correctos, se les asigna los defaults
     const colorChosen = color in theme.colors ? color : "default";
     const variantChosen = variantTypes.includes(variant) ? variant : "default"
     styleVariant = theme.colors[colorChosen][variantChosen]
 
   }
 
-  const styleShadow = variant === "default" && !disableShadow ? "drop-shadow-md" : ""
+  const styleShadow = variant === "default" && !disableShadow && !disable ? "drop-shadow-md" : ""
 
   return (
     <button className={`${styleGeneral} ${styleShadow} ${styleVariant}`} disabled={disable}>
--- app/page.js
+++ app/page.js
@@ -3,27 +3,42 @@ import StyledButton from "./components/StyledButton";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="w-9/12 flex flex-wrap gap-4">
-        <div>
-          <p className="text-black">default</p>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 text-black">
+      <div className="w-9/12 ">
+        <h1>Colores y estilos</h1>
+        <br />
+        <h2>Solid (default)</h2>
+        <div className="flex flex-wrap gap-4 w-full">
           <StyledButton />
+          <StyledButton color="primary" />
+          <StyledButton color="secondary" />
+          <StyledButton color="danger" />
         </div>
-        <div>
-          <p className="text-black">Disabled Shadow</p>
-          <StyledButton disableShadow />
+        <h2>Outline</h2>
+        <div className="flex flex-wrap gap-4 w-full">
+          <StyledButton variant="outline" />
+          <StyledButton variant="outline" color="primary" />
+          <StyledButton variant="outline" color="secondary" />
+          <StyledButton variant="outline" color="danger" />
         </div>
-        <div>
-          <p className="text-black">Outline</p>
-          <StyledButton variant="outline"/>
-        </div>
-        <div>
-          <p className="text-black">Text</p>
+        <h2>Text</h2>
+        <div className="flex flex-wrap gap-4 w-full">
           <StyledButton variant="text" />
+          <StyledButton variant="text" color="primary" />
+          <StyledButton variant="text" color="secondary" />
+          <StyledButton variant="text" color="danger" />
         </div>
-        <div>
-          <p className="text-black">Disabled</p>
-          <StyledButton disable text="Disabled" />
+        <br />
+        <h1>Sombras</h1>
+        <div className="flex flex-wrap gap-4 w-full">
+          <StyledButton text="Shadow" />
+          <StyledButton text="No shadow" disableShadow />
+        </div>
+        <br />
+        <h1>Deshabilitado</h1>
+        <div className="flex flex-wrap gap-4 w-full">
+          <StyledButton text="Habilitado" />
+          <StyledButton text="Deshabilitado" disable />
         </div>
       </div>
     </main>
